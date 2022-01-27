d07aff8def69c9fd8a4783ac7ed541972aec25e2
Michael-vdL <mvonderlippe@ansfederal.com>
2022-01-26 22:35:28 -0500

    More People Please

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,72 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import PeopleGrid from '../components/PeopleGrid/PeopleGrid';
-import StatementSection from '../components/StatementSection/StatementSection';
-import styles from '../styles/Home.module.css';
+import Head from 'next/head'
+import Image from 'next/image'
+import PeopleGrid from '../components/PeopleGrid/PeopleGrid'
+import StatementSection from '../components/StatementSection/StatementSection'
+import styles from '../styles/Home.module.css'
+import { AuroraWrapper, AuroraBall, AuroraFilter } from '../components/AuroraBackground/AuroraBall'
+import styled from 'styled-components'
+import { Carousel } from '../components/RotateAndFadeBro/Carousel'
+
+const MovedToAStyledComponent = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: '50px';
+  height: 100vh;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+  background-image: url('Desktop - 4.png');
+  background-repeat: no-repeat;
+  background-position: auto;
+  background-size: 100% 100%;
+`
+
+const TestImage = styled.div`
+  background-image: url(${(props) => props.src});
+  background-repeat: no-repeat;
+  background-position: cover;
+  background-size: 90%;
+  background-size: ${(props) => props.size};
+  width: 500px;
+  height: 240px;
+`
 
 export default function Home() {
+  // It uh... breaks after 5 items (maybe changing it so there are always less than 5 items and the items get pushed from a hidden array to a displayed array)
+  const items = [
+    <TestImage key={0} src='Girl.png' />,
+    <TestImage key={1} src='Girl.png' />,
+    <TestImage key={2} src='Girl.png' />,
+    <TestImage key={3} src='Girl.png' />,
+    <TestImage key={4} src='Girl.png' />,
+    <TestImage key={5} src='Girl.png' />,
+  ]
+
+  const settings = {
+    yOffset: '360px',
+    itemHeight: 240,
+    halfWayIndex: 3,
+    containerWidth: '600px',
+    containerHeight: '1000px',
+    rotationInterval: 1.5,
+  }
+
   return (
-    <div
-      style={{
-        display: 'flex',
-        flexDirection: 'row',
-        gap: '50px',
-        width: '100%',
-        // backgroundColor: 'rgba(248,225,204,1)',
-        justifyContent: 'center',
-        alignItems: 'flex-end',
-        overflow: 'hidden',
-        height: '100vh',
-      }}>
-      <StatementSection />
-      <PeopleGrid />
-    </div>
-  );
+    <>
+      {/* <AuroraWrapper>
+        <AuroraBall x={'-25%'} y={'-50%'} colorOne={'#E27F1C'} colorTwo={'rgba(226, 127, 28, 0)'} />
+        <AuroraBall x={'45%'} y={'-40%'} colorOne={'#D26900'} colorTwo={'rgba(226, 127, 28, 0)'} />
+        <AuroraBall x={'-30%'} y={'50%'} colorOne={'#FDEB52'} colorTwo={'rgba(226, 127, 28, 0)'} />
+        <AuroraFilter />
+      </AuroraWrapper> */}
+      {/* <MovedToAStyledComponent>
+        <PeopleGrid />
+      </MovedToAStyledComponent> */}
+      <MovedToAStyledComponent>
+        <StatementSection />
+        <Carousel items={items} {...settings} />
+      </MovedToAStyledComponent>
+    </>
+  )
 }
